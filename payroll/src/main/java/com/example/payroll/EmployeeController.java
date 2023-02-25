b6e8743ddcc80db54f54307e130eddbf6e2bb671
mckyhrs <af.18.715@gmail.com>
2023-02-25 17:15:06 +0900

    Spring HATEOASを追加して、EmployeeController#oneでEntityModelを使う

--- payroll/src/main/java/com/example/payroll/EmployeeController.java
+++ payroll/src/main/java/com/example/payroll/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.payroll;
 
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class EmployeeController {
@@ -35,8 +38,13 @@ public class EmployeeController {
     // Single item
 
     @GetMapping("/employees/{id}")
-    Employee  one(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id)
+            .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee,
+            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
