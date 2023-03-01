73c46150e005219f912e9fa9952a95ea6b7e7e25
mckyhrs <af.18.715@gmail.com>
2023-03-02 00:27:14 +0900

    EmployeeModelAssemblerを使ってEntityModelの作成処理を置き換える

--- payroll/src/main/java/com/example/payroll/EmployeeController.java
+++ payroll/src/main/java/com/example/payroll/EmployeeController.java
@@ -19,9 +19,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 class EmployeeController {
 
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
@@ -29,9 +31,7 @@ class EmployeeController {
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-            .map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+            .map(assembler::toModel)
             .collect(Collectors.toList());
 
         return CollectionModel.of(employees,
@@ -51,9 +51,7 @@ class EmployeeController {
         Employee employee = repository.findById(id)
             .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee,
-            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
