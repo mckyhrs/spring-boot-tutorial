e7ca5b7fc84849cce239adee83f9955097968e54
mckyhrs <af.18.715@gmail.com>
2023-03-07 17:55:32 +0900

    OrderControllerにcompleteメソッドを追加

--- payroll/src/main/java/com/example/payroll/OrderController.java
+++ payroll/src/main/java/com/example/payroll/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -67,5 +68,23 @@ class OrderController {
             .body(Problem.create()
                 .withTitle("Method not allowed")
                 .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
+    }
+
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+        Order order = orderRepository.findById(id)
+            .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+            .status(HttpStatus.METHOD_NOT_ALLOWED)
+            .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+            .body(Problem.create()
+                .withTitle("Method not allowd")
+                .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
     }
 }
